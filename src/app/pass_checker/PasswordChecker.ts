f0f6b9dd0f59f9c26e4176adb7249e0919ad7194
Ali salehi <parkour.runner@gmail.com>
2024-08-10 10:16:37 +0300

    iteration 3, expand test with private methods

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -2,6 +2,7 @@ export enum PasswordErrors {
   SHORT = "Password is too short",
   NO_UPPER_CASE = "Password needs to have upper case letter",
   NO_LOWER_CASE = "Password needs to have lower case letter",
+  NO_NUMBER = "At least one number required!",
 }
 
 export interface CheckResult {
@@ -12,15 +13,45 @@ export interface CheckResult {
 export class PasswordChecker {
   public checkPassword(pass: string): CheckResult {
     const reasons: PasswordErrors[] = [];
+
+    this.checkForLength(pass, reasons);
+    this.checkForUpperCase(pass, reasons);
+    this.checkForLowerCase(pass, reasons);
+
+    return { valid: reasons.length > 0 ? false : true, reasons };
+  }
+
+  public checkAdminPassword(pass: string): CheckResult {
+    const basicCheck = this.checkPassword(pass);
+    this.checkForNumber(pass, basicCheck.reasons);
+    return {
+      valid: basicCheck.reasons.length > 0 ? false : true,
+      reasons: basicCheck.reasons,
+    };
+  }
+
+  private checkForNumber(pass: string, reasons: PasswordErrors[]) {
+    const hasNumber = /\d/;
+    if (!hasNumber.test(pass)) {
+      reasons.push(PasswordErrors.NO_NUMBER);
+    }
+  }
+
+  private checkForLength(pass: string, reasons: PasswordErrors[]) {
     if (pass.length < 8) {
       reasons.push(PasswordErrors.SHORT);
     }
+  }
+
+  private checkForUpperCase(pass: string, reasons: PasswordErrors[]) {
     if (pass === pass.toLowerCase()) {
       reasons.push(PasswordErrors.NO_UPPER_CASE);
     }
+  }
+
+  private checkForLowerCase(pass: string, reasons: PasswordErrors[]) {
     if (pass === pass.toUpperCase()) {
       reasons.push(PasswordErrors.NO_LOWER_CASE);
     }
-    return { valid: reasons.length > 0 ? false : true, reasons };
   }
 }
