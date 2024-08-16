166c96f518d1f45241bf8fc5117d55628b9a0be7
Ali salehi <parkour.runner@gmail.com>
2024-08-16 11:08:33 +0300

    test doubles - fakes

--- src/app/doubles/OtherUtils.ts
+++ src/app/doubles/OtherUtils.ts
@@ -6,6 +6,21 @@ export type stringInfo = {
   extraInfo: Object | undefined;
 };
 
+type LoggerServiceCallBack = (arg: string) => void;
+
 export function calculateComplexity(stringInfo: stringInfo) {
   return Object.keys(stringInfo.extraInfo).length * stringInfo.length;
+}
+
+export function toUpperCaseWithCallBack(
+  arg: string,
+  cb: LoggerServiceCallBack
+) {
+  if (!arg) {
+    cb("Invalid argument!");
+    return;
+  }
+  cb("called function with " + arg);
+
+  return arg.toUpperCase();
 }
--- src/test/doubles/OtherUtils.test.ts
+++ src/test/doubles/OtherUtils.test.ts
@@ -1,18 +1,31 @@
-import { calculateComplexity } from "../../app/doubles/OtherUtils"
+import {
+  calculateComplexity,
+  toUpperCaseWithCallBack,
+} from "../../app/doubles/OtherUtils";
 
 describe("OtherUtils test suite", () => {
-  
+  it("ToUpperCase - calls callback with invalid arg", () => {
+    const actual = toUpperCaseWithCallBack("", () => {});
+
+    expect(actual).toBeUndefined
+  });
+  it("ToUpperCase - calls callback with valid arg", () => {
+    const actual = toUpperCaseWithCallBack("abc", () => {});
+
+    expect(actual).toBe("ABC")
+  });
+
   it("calculate complexity", () => {
     const someInfo = {
       length: 5,
       extraInfo: {
         field1: "info1",
         field2: "info2",
-      }
-    }
+      },
+    };
 
     const actual = calculateComplexity(someInfo as any);
 
     expect(actual).toBe(10);
-  })
-})
+  });
+});
