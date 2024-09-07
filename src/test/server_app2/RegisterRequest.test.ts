de49931cb97f00a1ed26241e298931d85bcb65ed
Ali salehi <parkour.runner@gmail.com>
2024-09-07 15:43:07 +0300

    low mock test

---
+++ src/test/server_app2/RegisterRequest.test.ts
@@ -0,0 +1,77 @@
+import { DataBase } from "../../app/server_app/data/DataBase";
+import {
+  HTTP_CODES,
+  HTTP_METHODS,
+} from "../../app/server_app/model/ServerModel";
+import { Server } from "../../app/server_app/server/Server";
+import { RequestTestWrapper } from "./test_utils/RequestTestWrapper";
+import { ResponseTestWrapper } from "./test_utils/ResponseTestWrapper";
+
+jest.mock("../../../app/server_app/data/DataBase");
+
+const requestWrapper = new RequestTestWrapper();
+const responseWrapper = new ResponseTestWrapper();
+
+const fakeServer = {
+  listen: () => {},
+  close: () => {},
+};
+
+jest.mock("http", () => ({
+  createServer: (cb: Function) => {
+    cb(requestWrapper, responseWrapper);
+    return fakeServer;
+  },
+}));
+
+describe("Register requests test suit", () => {
+  afterEach(() => {
+    requestWrapper.clearFields();
+    responseWrapper.clearFields();
+  });
+
+  it("should register new users", async () => {
+    requestWrapper.method = HTTP_METHODS.POST;
+    requestWrapper.body = {
+      userName: "someUserName",
+      password: "somePassword",
+    };
+    requestWrapper.url = "localhost:8080/register";
+
+    jest.spyOn(DataBase.prototype, "insert").mockResolvedValue("1234");
+    await new Server().startServer();
+    await new Promise(process.nextTick); // because of NodeJs cycle timing issue
+
+    expect(responseWrapper.statusCode).toBe(HTTP_CODES.CREATED);
+    expect(responseWrapper.body).toEqual(
+      expect.objectContaining({
+        userId: "1234",
+      })
+    );
+  });
+
+  it("should reject request with no username and password", async () => {
+    requestWrapper.method = HTTP_METHODS.POST;
+    requestWrapper.body = {};
+    requestWrapper.url = "localhost:8080/register";
+
+    await new Server().startServer();
+    await new Promise(process.nextTick); // because of NodeJs cycle timing issue
+
+    expect(responseWrapper.statusCode).toBe(HTTP_CODES.BAD_REQUEST);
+    expect(responseWrapper.body).toEqual("userName and password required");
+  });
+
+  it("should do nothing for not supported methods", async () => {
+    requestWrapper.method = HTTP_METHODS.DELETE;
+    requestWrapper.body = {};
+    requestWrapper.url = "localhost:8080/register";
+
+    await new Server().startServer();
+
+    await new Promise(process.nextTick); // this solves timing issues
+
+    expect(responseWrapper.statusCode).toBeUndefined();
+    expect(responseWrapper.body).toBeUndefined();
+  });
+});
